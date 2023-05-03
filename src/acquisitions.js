e3d17525df551897dfbc9f95682c88dcd0d61a2d
Polo <mphowitzer@gmail.com>
2023-05-02 20:51:23 -0600

    agregandoAPI

--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,21 +1,26 @@
 import Chart from 'chart.js/auto'
 import { incrementarValor } from './funciones';
+import { getAquisitionsByYear } from './api'
 
 (async function () {
-    const data = [
-        { year: 2010, count: 10 },
-        { year: 2011, count: 20 },
-        { year: 2012, count: 15 },
-        { year: 2013, count: 25 },
-        { year: 2014, count: 22 },
-        { year: 2015, count: 30 },
-        { year: 2016, count: 28 },
-    ].map(fila => ( { ...fila, count: incrementarValor(fila.count) }));
+    // const data = [
+    //     { year: 2010, count: 10 },
+    //     { year: 2011, count: 20 },
+    //     { year: 2012, count: 15 },
+    //     { year: 2013, count: 25 },
+    //     { year: 2014, count: 22 },
+    //     { year: 2015, count: 30 },
+    //     { year: 2016, count: 28 },
+    // ].map(fila => ( { ...fila, count: incrementarValor(fila.count) }));
+
+    const data = await getAquisitionsByYear();
 
     new Chart(
+
+        // bar, line, pie, doughnut, radar, polarArea, bubble
         document.getElementById('acquisitions'),
         {
-            type: 'bar',
+            type: 'bubble',
             data: {
                 labels: data.map(row => row.year),
                 datasets: [
